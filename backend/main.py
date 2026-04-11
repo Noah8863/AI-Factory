9fecbafae660cb6dfa3760bc43ec61b7a57da05d
Noah Hoffman <stacknoah99@gmail.com>
2026-04-11 12:37:50 -0600

    setting up railway URLs

--- backend/main.py
+++ backend/main.py
@@ -38,6 +38,8 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "http://127.0.0.1:5175",
+        "https://ai-factory-production-1f41.up.railway.app",
+        "https://ai-factory-production-bb66.up.railway.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
